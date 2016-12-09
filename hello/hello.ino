84ecd2736d916bb7aea992200d3378b62e4774d8
ponty <ponty@home>
2016-12-09 18:23:11 +0100

    hello: multiple baud

--- hello/hello.ino
+++ hello/hello.ino
@@ -2,7 +2,13 @@
 void setup() { 
  //Initialize serial and wait for port to open:
   Serial.begin(9600); 
-  Serial.println("hello!"); 
+  Serial.println(F("hello! (9600)")); 
+  Serial.end();
+  
+  Serial.begin(115200); 
+  Serial.println(F("hello! (115200)")); 
+  Serial.end();
+  
 } 
 void loop() { 
 } 
