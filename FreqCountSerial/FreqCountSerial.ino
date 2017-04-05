5cb5d6e175ec892353b1e027000c0a016908b36a
ponty <ponty@home>
2017-04-05 20:11:36 +0200

    FreqCountSerial

---
+++ FreqCountSerial/FreqCountSerial.ino
@@ -0,0 +1,20 @@
+/* FreqCount - Example with serial output
+ * http://www.pjrc.com/teensy/td_libs_FreqCount.html
+ *
+ * This example code is in the public domain.
+ */
+#include <FreqCount.h>
+
+void setup() {
+  Serial.begin(9600);
+  FreqCount.begin(1000); // gate time msec
+}
+
+void loop() {
+  if (FreqCount.available()) 
+  {
+    unsigned long count = FreqCount.read();
+    Serial.println(count);
+  }
+}
+
